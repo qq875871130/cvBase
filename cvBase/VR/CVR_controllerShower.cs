eedb45502605543e4afc9142c86c64f9b0304058
cwl <875871130@qq.com>
2022-01-01 15:16:09 +0800

    Reclassified

---
+++ cvBase/VR/CVR_controllerShower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace cvBase.VR
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CVR_controllerShower : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
