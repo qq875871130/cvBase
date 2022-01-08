d2af8d453b21888365c5e7659ec9b1a96d61037e
cwl <875871130@qq.com>
2022-01-09 02:56:15 +0800

    初步完成单链表与双链表 Single-Linked and Double-Linked List Preliminarily completed

--- cvBase/Type/cvType.cs
+++ cvBase/Type/cvType.cs
@@ -7,24 +7,44 @@ using System.Threading.Tasks;
 
     namespace cvBase.Type
     {
-        /// <summary>
-        /// 扩展bool，可监测变量变化以实现一次方法输出
-        /// </summary>
-        public class cvBool
+    /// <summary>
+    /// 扩展bool
+    /// <para>可监测变量变化以实现一次方法输出</para>
+    /// <para>在频繁调用函数体中使用可大大提高处理效率</para>
+    /// </summary>
+    public class bool_cv
         {
             private bool m_bool;
             private bool m_bool_tmp;   //缓存布尔  
-            public enum boolState   //是否生成同状态布尔
+            /// <summary>
+            /// 布尔对生成方法
+            /// </summary>
+            public enum boolState  
             {
+                /// <summary>
+                /// 布尔对一致
+                /// </summary>
                 same,
+                /// <summary>
+                /// 缓存布尔与初始设置布尔相反，用于使第一次change调用生效
+                /// </summary>
                 different
             }
-            public cvBool(bool b)
+            /// <summary>
+            /// 无参构造
+            /// </summary>
+            /// <param name="b">初始布尔值</param>
+            public bool_cv(bool b)
             {
                 m_bool = b;
                 m_bool_tmp = b;
             }
-            public cvBool(bool b, boolState state)
+           /// <summary>
+           /// 分类构造函数
+           /// </summary>
+           /// <param name="b">初始布尔</param>
+           /// <param name="state">布尔生成方式</param>
+            public bool_cv(bool b, boolState state)
             {
                 m_bool = b;
                 switch (state)
@@ -37,13 +57,19 @@ using System.Threading.Tasks;
                         break;
                 }
             }
-
+            /// <summary>
+            /// 设置变化布尔
+            /// </summary>
+            /// <param name="b">改变布尔值</param>
             public void setBool(bool b)
             {
                 m_bool_tmp = b;
             }
-
-
+            /// <summary>
+            /// 布尔变化判别
+            /// <para>用于函数体中按布尔调用，实现多次判断一次调用</para>
+            /// </summary>
+            /// <returns>布尔是否变化</returns>
             public bool isChange()
             {
                 //缓存布尔与初始布尔不同时，说明发生改变
@@ -54,8 +80,6 @@ using System.Threading.Tasks;
                 }
                 return false;
             }
-
         }
-
     }
 
--- cvTest/Program.cs
+++ cvTest/Program.cs
@@ -7,10 +7,10 @@ namespace cvTest
     {
         static void Main()
         {
-            LinkList<string> linkList = new();
+            LinkList<string>.Double linkList = new();
             for (int i = 1; i <= 10; i++)
             {
-                linkList.Add(i+"", LinkList<string>.AddType.backward);
+                linkList.Add(i + "", LinkList<string>.AddType.backward);
             }
             linkList.Add("8.5", 9, LinkList<string>.AddType.forward);
             linkList.Add("5.5", 5, LinkList<string>.AddType.backward);
@@ -27,19 +27,19 @@ namespace cvTest
             }
             Console.Write("\n");
             linkList.Delete("117");
-            linkList.Delete(1,5);
+            linkList.Delete(1, 5);
             ListAll<string>(linkList);
             linkList.Clear();
             ListAll<string>(linkList);
         }
 
-        public static void ListAll<T>(LinkList<T> linkList)
+        public static void ListAll<T>(LinkList<T>.Double linkList)
         {
             Console.WriteLine(linkList.Length);
             T[] list = linkList.ToArray();
             for (int i = 0; i < list.Length; i++)
             {
-                Console.WriteLine(i+1 + "\t" + list[i]);
+                Console.WriteLine(i + 1 + "\t" + list[i]);
             }
         }
     }
