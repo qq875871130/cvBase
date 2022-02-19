956ec83002815e040db3c5896a179014b31136b7
cwl <875871130@qq.com>
2022-02-20 00:14:06 +0800

    删除主入口原始冗余测试代码 Delete Original Redundant Code In Main Entrance Program

--- cvTest/Program.cs
+++ cvTest/Program.cs
@@ -11,30 +11,6 @@ namespace cvTest
             string path = "..//..//..//DS/zh-CN.xml";
             CmdProcessor cmdProcessor = new CmdProcessor(path);
             cmdProcessor.Start();
-            //LinkList<string>.Double linkList = new();
-            //for (int i = 1; i <= 10; i++)
-            //{
-            //    linkList.Add(i + "", LinkList<string>.AddType.backward);
-            //}
-            //linkList.Add("8.5", 9, LinkList<string>.AddType.forward);
-            //linkList.Add("5.5", 5, LinkList<string>.AddType.backward);
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    linkList.Add("117", LinkList<string>.AddType.backward);
-            //}
-
-            ////输出流
-            //ListAll<string>(linkList);
-            //foreach (var index in linkList.GetIndexes("117"))
-            //{
-            //    Console.Write(index + "\t");
-            //}
-            //Console.Write("\n");
-            //linkList.Delete("117");
-            //linkList.Delete(1, 5);
-            //ListAll<string>(linkList);
-            //linkList.Clear();
-            //ListAll<string>(linkList);
         }
 
         public static void ListAll<T>(List.LinkList<T>.Double linkList)
